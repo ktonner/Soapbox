56b98e6302f8df3dee03098f10263709809f17ae
Yoom Davidson <ddavidson202020@gmail.com>
2020-05-12 14:26:16 -0400

    Set up postsController and routes

--- models/account.js
+++ models/account.js
@@ -6,7 +6,11 @@ const Account = new Schema({
     username: String,
     password: String
 });
-
+Account.associate = function(models) {
+    Account.hasMany(models.Post, {
+      onDelete: "cascade"
+    });
+  };
 Account.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('accounts', Account);
---
+++ models/post.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Post = new Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    text: String,
+    date: { type: Date, default: Date.now }
+});
+Post.associate = function(models) {
+    Post.belongsTo(models.Account, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+module.exports = mongoose.model('posts', Post);
