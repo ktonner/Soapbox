d4f2f27e5bfbdac677bc7168d7ad20c3c8dd2ae8
Kathleen Tonner <kathleentonner@gmail.com>
2020-05-13 14:59:42 -0400

    Full post showing, tags included

--- client/src/components/Post/index.js
+++ client/src/components/Post/index.js
@@ -5,29 +5,37 @@ import Card from 'react-bootstrap/Card'
 
 const Post = () => {
 
+    //setting up state, Post info comes in here
     const [postState, setPostState] = useState({
         date: Date,
         title: "Test",
         text: "test",
-        tags: "test"
+        tags: ["test", "one", "two"]
     })
 
+    //this is the map loop for the tags
+    const tagList = postState.tags
+    const tagItems = tagList.map(tag =>{
         return(
-        <Card>
-            <Card.Header className="text-muted">{postState.date}</Card.Header>
-            <Card.Body>
-                <Card.Title>{postState.title}</Card.Title>
-                <Card.Text>
-                    {postState.text}
-    </Card.Text>
-            </Card.Body>
-            <Card.Footer className="text-muted">
-                {postState.tags}.map((tag)=>{
-                    <p>"#"+tag+" "</p>
-                })
-            </Card.Footer>
-        </Card>
-        )    
+            <p style={{display:"inline"}}>#{tag} </p>
+        )
+    })
+
+
+return (
+    <Card>
+        <Card.Header className="text-muted">{postState.date}</Card.Header>
+        <Card.Body>
+            <Card.Title>{postState.title}</Card.Title>
+            <Card.Text>
+                {postState.text}
+            </Card.Text>
+        </Card.Body>
+        <Card.Footer className="text-muted">
+                {tagItems}
+        </Card.Footer>
+    </Card>
+)    
 }
 
 export default Post
