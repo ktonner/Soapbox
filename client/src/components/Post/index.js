600f7383bf981728251491aa8de6e9f35d4d142c
Kathleen Tonner <kathleentonner@gmail.com>
2020-05-14 11:12:46 -0400

    switched to state

--- client/src/components/Post/index.js
+++ client/src/components/Post/index.js
@@ -1,41 +1,61 @@
-import React, { useState, useContext } from "react";
+import React, { Component, useState, useContext } from "react";
 import Card from 'react-bootstrap/Card'
 
 
 
+  //creating context
+  const PostContext = React.createContext()
+
+  class PostProvider extends Component {
+      state = {
+          date: Date.now(),
+          author: "ktonner",
+          title: "Test",
+          text: "test",
+          tags: ["test", "one", "two"]
+      }
+      render() {
+          return (
+              <PostContext.Provider value={{
+                  state: this.state
+              }}>
+                  {this.props.children}
+              </PostContext.Provider>
+          )
+      }
+  };
+
+
+
+
 const Post = () => {
-
-    //setting up state, Post info comes in here
-    const [postState, setPostState] = useState({
-        date: Date,
-        title: "Test",
-        text: "test",
-        tags: ["test", "one", "two"]
-    })
-
-    //this is the map loop for the tags
-    const tagList = postState.tags
-    const tagItems = tagList.map(tag =>{
-        return(
-            <p style={{display:"inline"}}>#{tag} </p>
-        )
-    })
-
-
-return (
-    <Card>
-        <Card.Header className="text-muted">{postState.date}</Card.Header>
-        <Card.Body>
-            <Card.Title>{postState.title}</Card.Title>
-            <Card.Text>
-                {postState.text}
-            </Card.Text>
-        </Card.Body>
-        <Card.Footer className="text-muted">
-                {tagItems}
-        </Card.Footer>
-    </Card>
-)    
+    return (
+        <PostProvider>
+        <PostContext.Consumer>
+            {(context) => (
+                <Card>
+                    <Card.Header className="text-muted">{context.state.date}</Card.Header>
+                    <Card.Body>
+                        <Card.Title>{context.state.title}</Card.Title>
+                        <Card.Text>
+                            {context.state.text}
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer className="text-muted">
+                        {
+                            //Map loops over tags and formats them
+                            (context.state.tags).map(tag => {
+                                return (
+                                    <p style={{ display: "inline" }}>#{tag} </p>
+                                )
+                            })
+                        }
+                    </Card.Footer>
+                </Card>
+            )}
+        </PostContext.Consumer>
+        </PostProvider>
+    )
 }
 
 export default Post
