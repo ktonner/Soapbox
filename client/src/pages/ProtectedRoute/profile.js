42e8aa1791abdcacf1c4f19ab283f7522bf11e91
Yoom Davidson <ddavidson202020@gmail.com>
2020-05-13 20:47:41 -0400

    Apply card styling for the post and add Update and Delete buttons

--- client/src/pages/ProtectedRoute/profile.js
+++ client/src/pages/ProtectedRoute/profile.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext,useState } from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 //import { UserContext } from "../../utils/UserContext";
 import API from "../../utils/postsAPI"
 
@@ -6,52 +6,56 @@ import API from "../../utils/postsAPI"
 
 function Profile() {
     //const [user, dispatch] = useContext(UserContext);
-    
+
     const [state, setPosts] = useState({
         title: "",
         text: "",
         tags: "",
         posts: []
     })
-	
+
     useEffect(() => {
         API.getPosts()
-            .then(res=> {
+            .then(res => {
                 console.log("This is the data", res)
-                setPosts({posts: res.data})
+                setPosts({ posts: res.data })
             })
             .catch(err => {
                 console.log(err)
             })
-    },[]);
+    }, []);
     const displayPost = (posts) => {
-        if(!posts.length) return null;
-        return posts.map((post,index) => (
-        
-            <div key={index}>
-                <h5>Title: {post.title}</h5>
-                <p>Content: {post.text}</p>
-                <p>#{post.tags}</p>
+        if (!posts.length) return null;
+        return posts.map((post, index) => (
+
+            <div key={index} className="card">
+                <div className="card-body">
+                    <h5 className="card-title">Title: {post.title}</h5>
+                    <p className="card-subtitle mb-2 text-muted">#{post.tags}</p>
+                    <p className="card-text">Content: {post.text}</p>
+                    <button className="btn btn-primary">Update</button><span> </span>
+                    <button className="btn btn-danger">Delete</button>
+                </div>
             </div>
-        
+
         ));
     }
-	return (
-		<div className="container">
-			<div className="row">
+    return (
+        <div className="container">
+            <div className="row">
                 <div className="col-md-6">
                     <h4>List of your posts</h4>
-                    <br/>
+                    <br />
                     {displayPost(state.posts)}
                 </div>
                 <div className="col-md-6">
                     <h4>List of people you have followed</h4>
-                    
+
                 </div>
             </div>
-		
-		</div>
-	)
+
+        </div>
+    )
 
 }
 
