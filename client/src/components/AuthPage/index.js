e35ad33689b55026a806dcb8d22d9abb2216cedf
Yoom Davidson <ddavidson202020@gmail.com>
2020-05-17 15:21:27 -0400

    Change app name

--- client/src/components/AuthPage/index.js
+++ client/src/components/AuthPage/index.js
@@ -4,7 +4,7 @@ function AuthPage() {
     return (
        <div style={{backgroundImage: `url(${image})`}}>
          <div style={{paddingBottom: "50%", paddingLeft: "20px", paddingRight: "60%"}}>
-                    <h3 style={{paddingTop: "50px"}}>Welcome to our Social Media Page!</h3>
+                    <h3 style={{paddingTop: "50px"}}>Welcome to Soapbox!</h3>
                     <h5>This is the place where you can sign up to be a member to see all posts from all of our users, you can search for posts by hashtag#, create your own post, update or delete your posts. One of our special features is that you can follow other users to see their posts. Let's start by logging in or register a new user on your top right corner!💖</h5>
                 </div>
         </div>
