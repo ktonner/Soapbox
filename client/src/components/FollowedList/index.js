66b72903a141f956305e4d52b43f096506ab1a26
Kathleen Tonner <kathleentonner@gmail.com>
2020-05-17 18:17:06 -0400

    it should be showing

--- client/src/components/FollowedList/index.js
+++ client/src/components/FollowedList/index.js
@@ -1,39 +1,45 @@
 import React, { useEffect, useState } from "react"
 import { getUser, getUserFromID } from "../../utils/accountsAPI"
+import ListGroup from 'react-bootstrap/ListGroup'
 
-function FollowedList () {
+function FollowedList() {
 
-    const [following, setFollowing] = useState([])
+    const [followings, setFollowing] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         loadList()
-    },[])
+    }, [])
 
     function loadList() {
+        const array = []
         getUser().then(res => {
             console.log(res.data.following)
-            setFollowing(res.data.following)
+            res.data.following.map((account) => {
+                getUserFromID(account)
+                .then(res => {
+                    followings.push(res.data.username)
+                })
+            })
+            console.log(followings)
         })
-        .catch(err => console.log(err));
+            .catch(err => console.log(err));
     };
 
 
 
-  return(
-            <ul>
-            {following.map((account, index) => {
-                getUserFromID(account).then(res => {
-                    console.log(res)
-                    const followingUser = res.data.username
-                    console.log(followingUser)
+    return (
+        <div>
+            <h4>test</h4>
+            <ListGroup>
+                {followings.map(account => {
                     return (
-                        
-                        <li key={index}>{followingUser}</li>
-        )
-                    })})}
-                    </ul>
-        )
-    }
+                        <ListGroup.Item>{account}</ListGroup.Item>
+                    )
+                })}
+            </ListGroup>
+        </div>
+    )
+}
 
 
 export default FollowedList
