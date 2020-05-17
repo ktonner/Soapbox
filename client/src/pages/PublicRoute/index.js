5b6cc9e64ed2e3f4950790e3d8f2aebc19c740a6
Yoom Davidson <ddavidson202020@gmail.com>
2020-05-17 12:32:51 -0400

    Add AuthPage component and style it

--- client/src/pages/PublicRoute/index.js
+++ client/src/pages/PublicRoute/index.js
@@ -12,9 +12,9 @@ function PublicRoute() {
                 
                 <div style={{paddingBottom: "50%", paddingLeft: "30%"}}>
                     <h3 style={{paddingTop: "200px"}}>Welcome to our Social Media Page!</h3>
-                    <h5>This is the place where you can sign up to be a member to see all posts from all of our users, you can search for posts by hashtag#, create your own post, update or delete your posts. One of our special features is that you can follow other users to see their posts.</h5>
+                    <h5>This is the place where you can sign up to be a member to see all posts from all of our users, you can search for posts by hashtag#, create your own post, update or delete your posts. One of our special features is that you can follow other users to see their posts. Let's start by logging in or register a new user on your top right corner!💖</h5>
                 </div>
-                
+                 
             
         </div>
     
