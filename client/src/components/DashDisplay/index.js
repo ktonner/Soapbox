49e878f181a86d8989c8f0ac67304d52d191cbc0
Kathleen Tonner <kathleentonner@gmail.com>
2020-05-18 12:30:18 -0400

    fiddling

--- client/src/components/DashDisplay/index.js
+++ client/src/components/DashDisplay/index.js
@@ -1,3 +1,39 @@
 import React, { useEffect, useContext, useState, Component } from 'react'
 import API from "../../utils/postsAPI"
 import Post from "../Post/index"
+
+class DashDisplay extends React.Component {
+
+    state= {
+        search: "",
+        posts:[],
+        filteredPosts: []
+    };
+
+    componentDidMount() {
+       API.getUser().then(res=>{
+           console.log(res)
+           console.log(res.id)
+       })
+    }
+
+    render() {
+        return (
+            <div>
+            <br/><br/>
+            {this.state.filteredPosts.map((post, index) => {
+                {console.log(this.state.posts)}
+                {console.log(post)}
+                {console.log(post.authorID)}
+                return(
+                <Post key={post.authorID} authorID={post.authorID} date={post.date} author={post.author} title={post.title} text={post.text} tags={post.tags} />
+                )
+            })}
+
+            </div>
+        )
+    }
+}
+
+export default DashDisplay
+
