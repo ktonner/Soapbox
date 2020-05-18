8b4fe666f1672c0c4831d8bf5eba03fef8032089
Kathleen Tonner <kathleentonner@gmail.com>
2020-05-18 15:01:55 -0400

    figuring out post format

--- client/src/components/DashDisplay/index.js
+++ client/src/components/DashDisplay/index.js
@@ -1,45 +1,53 @@
 import React, { useEffect, useContext, useState, Component } from 'react'
-import {getUser, getUserFromID} from "../../utils/accountsAPI"
+import { getUser, getUserFromID } from "../../utils/accountsAPI"
 import Post from "../Post/index"
-const API = require("../../utils/postsAPI")
+import API from "../../utils/postsAPI"
 
 class DashDisplay extends React.Component {
 
-    state= {
+    state = {
         posts: []
     };
 
     componentDidMount() {
-        const postsArray=[]
-       getUser().then(res=>{
-           res.data.following.map(account=>{
-               getUserFromID(account).then(res=>{
-                   res.data.posts.map(post=>{
-                       postsArray.push(post)
-                   })
-                   this.setState({posts: postsArray})
-                   console.log(this.state.posts)
-                   
-               })
-           })
-       })
-    }
+        const postsArray = []
+        getUser().then(res => {
+            res.data.following.map(account => {
+                getUserFromID(account).then(res => {
+                    res.data.posts.map(post => {
+                        postsArray.push(post)
+                    })
+                    this.setState({ posts: postsArray })
+                    console.log(this.state.posts)
 
-    createPosts(){
-        this.state.posts.map(post=>{
-            API.getPost(post)
-            console.log(post)
-            return(
-                <Post key={post.authorID} authorID={post.authorID} date={post.date} author={post.author} title={post.title} text={post.text} tags={post.tags} />
-                )
+                })
+            })
+        })
+        this.state.posts.map(post => {
+            API.getPost(post).then(res=>{
+            console.log(res)
+            return (
+                <Post key={res.authorID} authorID={res.authorID} date={res.date} author={res.author} title={res.title} text={res.text} tags={res.tags} />
+              )})
         })
     }
 
+    createPosts() {
+        this.state.posts.map(post => {
+        API.getPost(post).then(res=>{
+        console.log(res)
+        return (
+            <Post key={res.authorID} authorID={res.authorID} date={res.date} author={res.author} title={res.title} text={res.text} tags={res.tags} />
+          )})
+    })
+
+    }
+
     render() {
-        
+
         return (
             <div>
-                {this.createPosts()}
+           
             </div>
         )
     }
