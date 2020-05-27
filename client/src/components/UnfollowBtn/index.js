ce322aab09cdc9c4bcdd45647c0afac35fe00809
Kathleen Tonner <kathleentonner@gmail.com>
2020-05-27 18:53:26 -0400

    unfollow btn external component

---
+++ client/src/components/UnfollowBtn/index.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react"
+import Button from 'react-bootstrap/Button'
+
+
+function UnfollowBtn() {
+return(
+<Button class="float-right" variant="danger">Unfollow</Button>
+)
+}
+
+export default UnfollowBtn
