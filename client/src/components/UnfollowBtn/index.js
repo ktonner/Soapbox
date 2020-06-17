f969296fe34d402dea15e2f2c0502c87a1bf6a06
Kathleen Tonner <kathleentonner@gmail.com>
2020-06-17 12:58:55 -0400

    unfollowing sets button to disabled

--- client/src/components/UnfollowBtn/index.js
+++ client/src/components/UnfollowBtn/index.js
@@ -6,14 +6,19 @@ import { handleUnfollow, removeFollower } from '../../utils/accountsAPI'
 
 function UnfollowBtn(props) {
 
+   const [disabled, setDisabled] = useState(false)
+   const [btn, setBtn] = useState("Unfollow")
+
 const handleClick = (id) => {
    console.log(id)
    handleUnfollow(id)
    removeFollower(id)
+   setDisabled(true)
+   setBtn("Unfollowed")
 }
 
 return(
-<Button className="float-right" variant="danger" disabled={false} id={props.id} onClick={() => handleClick((props.id))}>Unfollow</Button>
+<Button className="float-right" variant="danger" disabled={disabled} id={props.id} onClick={() => handleClick((props.id))}>{btn}</Button>
 )
 }
 
